37cecbf87f6d231a3db8b858a800dc51ed499021
hitorigoya <75735750+hitorigoya@users.noreply.github.com>
2023-10-25 17:05:12 +0900

    pre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import wasm from 'vite-plugin-wasm';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: false },
+  css: ['~/assets/css/main.css'],
+  vite: {
+    plugins: [wasm()],
+  },
 })
